a8f6764fd396724d5dc7b2b8081959ceaa104dd8
Ilias <77877193+iliaszach@users.noreply.github.com>
2021-06-24 10:38:09 +0300

    Add project files.

---
+++ Traffic_light/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Traffic_light
+{
+    class Program
+    {
+        //green -> yellow ->red
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UpdateLight("green"));
+        }
+
+
+        public static string UpdateLight2(string current)
+        {
+            switch (current)
+            {
+                case "green": return "yellow";    
+                case "yellow": return "red";   
+                case "red": return "green";
+                
+            }
+            return current;
+            
+        }
+        public static string UpdateLight(string current)
+        {
+            return current == "green" ? "yellow" : current == "yellow" ? "red" : "green";
+            
+        }
+    }
+}
